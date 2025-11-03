e0024d5e457f722fa590c8007ed551bb2f93419c
Nishant Rayan <nishant@Nishants-MacBook-Air.local>
2025-11-03 13:50:50 -0800

    Implement MemTable::create() and fix get() method

--- mini-lsm-starter/src/mem_table.rs
+++ mini-lsm-starter/src/mem_table.rs
@@ -52,8 +52,13 @@ pub(crate) fn map_bound(bound: Bound<&[u8]>) -> Bound<Bytes> {
 
 impl MemTable {
     /// Create a new mem-table.
-    pub fn create(_id: usize) -> Self {
-        unimplemented!()
+    pub fn create(id: usize) -> Self {
+        Self {
+            id,
+            map: Arc::new(SkipMap::new()),
+            wal: None,
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        }
     }
 
     /// Create a new mem-table with WAL
@@ -87,7 +92,8 @@ impl MemTable {
 
     /// Get a value by key.
     pub fn get(&self, _key: &[u8]) -> Option<Bytes> {
-        unimplemented!()
+        let key_bytes = Bytes::copy_from_slice(_key);
+        self.map.get(&key_bytes).map(|entry| entry.value().clone())
     }
 
     /// Put a key-value pair into the mem-table.
@@ -96,7 +102,9 @@ impl MemTable {
     /// In week 2, day 6, also flush the data to WAL.
     /// In week 3, day 5, modify the function to use the batch API.
     pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+        self.map
+            .insert(Bytes::copy_from_slice(_key), Bytes::copy_from_slice(_value));
+        Ok(())
     }
 
     /// Implement this in week 3, day 5; if you want to implement this earlier, use `&[u8]` as the key type.
